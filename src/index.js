716569037fb76dd0407713bbdbdce4ac4287ef6c
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2018-02-23 15:35:25 -0500

    why wont index appear?

--- src/index.js
+++ src/index.js
@@ -41,15 +41,11 @@ controller.setupWebserver(port, (err, webserver) => {
   controller.createWebhookEndpoints(controller.webserver)
   controller.createOauthEndpoints(controller.webserver, (authErr, req, res) => {
     if (authErr) res.status(500).send(`ERROR: ${authErr}`)
-    else res.send('Success! Problem Bot (beta) has been added to your team')
+    else res.send('Success! Problem Bot has been added to your team')
   })
 
   webserver.get('/', (req, res) => {
-    res.send('<a href="https://slack.com/oauth/authorize?scope=bot&' +
-      'client_id=64177576980.310915268453"><img alt="Add to Slack" ' +
-      'height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" ' +
-      'srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,' +
-      'https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>')
+    res.sendFile('/index.html')
   })
 
   webserver.get('/login/:slackUserId', auth.login)
